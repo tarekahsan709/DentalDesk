612c8be4220a2a6953428d908dcb07418faf8202
Cirko <dragonmen@gmail.com>
2023-01-22 21:53:49 +0100

    Correct paths for batch file

--- build-prod.bat
+++ build-prod.bat
@@ -1,14 +1,12 @@
 @echo off
 
 rem app.js and index.html
-node_modules/.bin/webpack -p --env.production --mode production
+node_modules\.bin\webpack -p --env.production --mode production
 echo "WebPack completed successfully"
 
-pause
-
 rem build css
-./node_modules/.bin/node-sass ./src/styles/index.scss ./dist/application/style.css
+node_modules\.bin\node-sass src\styles\index.scss \dist\application\style.css
 
 rem minify css
-./node_modules/.bin/uglifycss ./dist/application/style.css --output ./dist/application/style.css
+.\node_modules\.bin\uglifycss .\dist\application\style.css --output .\dist\application\style.css
 echo "Sass built and completed successfully"
