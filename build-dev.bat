8393101efacb3a316ddf0cc8b46fb0084ffcc860
Cirko <dragonmen@gmail.com>
2023-02-07 22:49:30 +0100

    Builds

---
+++ build-dev.bat
@@ -0,0 +1,14 @@
+@echo off
+
+rem app.js and index.html
+node_modules\.bin\webpack -p --env.production --mode production
+echo "WebPack completed successfully"
+
+rem build css
+node_modules\.bin\node-sass src\styles\index.scss \dist\application\style.css
+
+rem minify css
+.\node_modules\.bin\uglifycss .\dist\application\style.css --output .\dist\application\style.css
+echo "Sass built and completed successfully"
+
+pause
--- build-prod.bat
+++ build-prod.bat
@@ -1,13 +1,15 @@
 @echo off
 
-node_modules\.bin\webpack -p --env.production --mode production
-echo "WebPack completed successfully"
+echo Starting builds
 
-node_modules\.bin\node-sass src\styles\index.scss \dist\application\style.css
-echo "SCSS built successfully"
+@node_modules\.bin\webpack -p --env.production --mode production
+echo WebPack completed successfully
 
- node_modules\.bin\uglifycss .\dist\application\style.css --output .\dist\application\style.css
-echo "CSS uglify completed successfully"
+@node_modules\.bin\node-sass src\styles\index.scss \dist\application\style.css
+echo SCSS built successfully
+
+@node_modules\.bin\uglifycss .\dist\application\style.css --output .\dist\application\style.css
+echo CSS uglify completed successfully
 
 del dist\application\_session
 
