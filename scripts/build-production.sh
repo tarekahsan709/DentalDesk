4b06f3edfac4d95b5ee9374711e7e34b2233cad9
nazmul <tarekahsan709@gmail.com>
2024-05-10 23:44:38 +0200

    update prod sh

--- scripts/build-production.sh
+++ scripts/build-production.sh
@@ -2,7 +2,7 @@
 node_modules/.bin/webpack -p --env.production --mode production &&
 echo "WebPack completed successfully" &&
 # build css
-./node_modules/.bin/node-sass ./src/styles/index.scss ./dist/application/style.css &&
+./node_modules/.bin/node-sass ./src/styles/index.scss ./dist/style.css &&
 # minify css
-./node_modules/.bin/uglifycss ./dist/application/style.css --output ./dist/application/style.css &&
+./node_modules/.bin/uglifycss ./dist/style.css --output ./dist/style.css &&
 echo "Sass built and completed successfully"
