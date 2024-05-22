0e5ba097c41d6ef4c68fc7bdda668b4c4c3d44db
nazmul <tarekahsan709@gmail.com>
2024-05-22 20:26:30 +0200

    fix icons

--- scripts/build-development.sh
+++ scripts/build-development.sh
@@ -1,8 +1,8 @@
 # lint typescript
 bash scripts/lint.sh &
 # build app.js file and copy index.html
-node_modules/.bin/webpack --config webpack.config.dev.js --mode development -w &&
+node_modules/.bin/webpack --config webpack.config.dev.js --mode development -w &
 # build scss
-./node_modules/.bin/node-sass ./src/styles/index.scss ./dist/style.css &&
+./node_modules/.bin/node-sass ./src/styles/index.scss ./dist/style.css &
 # watch scss
 ./node_modules/.bin/node-sass ./src/styles/index.scss ./dist/style.css -w
